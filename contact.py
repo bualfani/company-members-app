42824c0b085610582b1175624c05424677d6515f
bahati <bulozealfani@gmail.com>
2023-02-16 23:35:22 -0600

    created contact page, added columns

---
+++ contact.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas
+
+with st.form('form1', clear_on_submit=True):
+    st.text_input("Your Email Address")
+    st.selectbox("What topic do you want to discuss?", pandas.read_csv('topics.csv'))
+    st.text_area("Text")
+
+    st.form_submit_button("Send")
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas
 
+st.set_page_config(layout="wide" )
+
+st.sidebar.title('Home')
+st.sidebar.title("Contact Us")
+
 st.title("The Company")
 content = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
@@ -19,4 +24,20 @@ col1, col2, col3, col4, col5 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.header(row['first name', 'last name'])
+        st.header(row['first name'] + ' ' + row['last name'])
+        st.write(row['role'])
+        st.image("image1/" + row['image'])
+
+
+with col3:
+    for index, row in df[4:8].iterrows():
+        st.header(row['first name'] + ' ' + row['last name'])
+        st.write(row['role'])
+        st.image("image1/" + row['image'])
+
+
+with col5:
+    for index, row in df[8:12].iterrows():
+        st.header(row['first name'] + ' ' + row['last name'])
+        st.write(row['role'])
+        st.image("image1/" + row['image'])
