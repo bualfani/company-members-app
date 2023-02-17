f865ca8b2d5666eeb3b7e6a0d39c5456dcd69c9d
bahati <bulozealfani@gmail.com>
2023-02-16 23:56:35 -0600

    capitalized the names and added page directory

--- Home.py
+++ Home.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas
 
-st.set_page_config(layout="wide" )
-
-st.sidebar.title('Home')
-st.sidebar.title("Contact Us")
+st.set_page_config(layout="wide")
 
 st.title("The Company")
 content = """
@@ -16,7 +13,7 @@ non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 """
 st.write(content)
 
-st.subheader("Our Team")
+st.header("Our Team")
 
 df = pandas.read_csv('data1.csv', sep=',')
 
@@ -24,20 +21,20 @@ col1, col2, col3, col4, col5 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.header(row['first name'] + ' ' + row['last name'])
+        st.subheader(f"{row['first name'].title()}  {row['last name'].title()}")
         st.write(row['role'])
         st.image("image1/" + row['image'])
 
 
 with col3:
     for index, row in df[4:8].iterrows():
-        st.header(row['first name'] + ' ' + row['last name'])
+        st.subheader(f"{row['first name'].title()}  {row['last name'].title()}")
         st.write(row['role'])
         st.image("image1/" + row['image'])
 
 
 with col5:
     for index, row in df[8:12].iterrows():
-        st.header(row['first name'] + ' ' + row['last name'])
+        st.subheader(f"{row['first name'].title()}  {row['last name'].title()}")
         st.write(row['role'])
         st.image("image1/" + row['image'])
--- pages/contact.py
+++ pages/contact.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas
 
+st.set_page_config(layout="wide" )
+
 with st.form('form1', clear_on_submit=True):
     st.text_input("Your Email Address")
     st.selectbox("What topic do you want to discuss?", pandas.read_csv('topics.csv'))
